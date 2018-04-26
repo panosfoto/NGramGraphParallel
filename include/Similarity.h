cf2a6a83e859c6dae3514105b3f4c1ff6c7ace28
Panagiotis Fotopoulos <panos.foto@hotmail.com>
2018-04-26 18:39:24 +0300

    Added Similarity (abstract) class

---
+++ include/Similarity.h
@@ -0,0 +1,28 @@
+#ifndef SIMILARITY_H
+#define SIMILARITY_H
+
+#include <map>
+using namespace std;
+
+/**
+ * \Class A class that represents a generic similarity interface
+ */
+class Similarity
+{
+    public:
+        /** \brief
+         *      Used to get a collection of the similarities defined in sub-class with their values
+         * \return
+         *      Returns a map which contains pairs like <SimilarityName, SimilarityValue>
+         */
+        virtual map<string, double> getSimilarityComponents() = 0;
+
+        /** \brief
+         *      Used to get the overall similarity, as it is defined in the sub-class
+         * \return
+         *      Returns the overall similarity
+         */
+        virtual double getOverallSimilarity() = 0;
+};
+
+#endif // SIMILARITY_H
