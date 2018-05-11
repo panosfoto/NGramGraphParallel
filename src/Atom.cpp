04185093edd6c0ad7e6c83996040d975c469c945
Panagiotis Fotopoulos <panos.foto@hotmail.com>
2018-05-11 23:05:44 +0300

    temporarily disabled toString() template spacialization for strings due to some errors, will be resolved

--- src/Atom.cpp
+++ src/Atom.cpp
@@ -1,15 +1,28 @@
 #include "Atom.hpp"
 
 
-
+//
 // template specialization for string atoms
+
 template <>
 bool Atom<std::string>::operator==(const Atom& other)
 {
     return this->payload.compare(other.payload) == 0;
 }
 
+
+/*
+template <>
+std::string Atom<std::string>::toString()
+{
+    return payload;
+}
+*/
+
+
+//
 // template specialization for integer atoms
+
 template <>
 bool Atom<int>::operator==(const Atom& other)
 {
