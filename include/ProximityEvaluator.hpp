6e657d0ae3f13fb7d16837c6976dee71f1298c29
Panagiotis Fotopoulos <panos.foto@hotmail.com>
2018-06-04 17:03:41 +0300

    added ProximityEvaluator abstract class.

---
+++ include/ProximityEvaluator.hpp
@@ -0,0 +1,34 @@
+/**
+ * \file ProximityEvaluator.hpp
+ */
+
+#ifndef PROXIMITYEVALUATOR_H
+#define PROXIMITYEVALUATOR_H
+
+
+// project headers
+#include "Atom.hpp"
+
+
+/**
+ * \Class An abstract class that describes a class that can evaluate if two Atoms are close.\n
+ * Usage: Every ProximityGraph holds a ProximityEvaluator. When the ProximityGraph creates its graph, the ProximityEvaluator is used to identify which Atoms will be connected.
+ */
+template <typename AtomType>
+class ProximityEvaluator
+{
+    public:
+        /** Evaluates whether the 2 given atoms are "close". \n
+         * Subclasses will define "close" depending on AtomType and desired functionality.
+         *  \param atomA The first atom.
+         *  \param atomB The second atom.
+         */
+        bool areClose(Atom atomA, Atom atomB) = 0;
+};
+
+
+// template implementation file
+#include "ProximityEvaluator.tcc"
+
+
+#endif // PROXIMITYEVALUATOR_H
