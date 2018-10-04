81aa06979a78708e4df0aae2554cf7bc2ca2c8ee
Panagiotis Fotopoulos <panos.foto@hotmail.com>
2018-10-04 07:46:30 +0300

    Updated missing documentation in graph comparator

--- include/GraphComparator.hpp
+++ include/GraphComparator.hpp
@@ -26,7 +26,7 @@
 
 /**
  * \Class
- * Usage:
+ * A class that compares ProximityGraphs. It is type-agnostic, so the types are templated.
  */
 template <typename PayloadType, typename AtomType>
 class GraphComparator
@@ -42,6 +42,11 @@ class GraphComparator
 
 
 
+        /** Compares two ProximityGraphs. The result is returned in the special class designed for this reason, GraphSimilarity.
+         * \param pGraph1 a reference to a ProximityGraph
+         * \param pGraph2 a reference to a ProximityGraph
+         * \return The similarity between the two graphs, in GraphSimilarity format.
+         */
         virtual GraphSimilarity compare(ProximityGraph<PayloadType, AtomType> &pGraph1, ProximityGraph<PayloadType, AtomType> &pGraph2);
 
     protected:
